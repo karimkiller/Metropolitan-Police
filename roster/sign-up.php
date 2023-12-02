e0813ad444a8435d2859f791175e76f11b833388
karimkiller <karimadel66.ka@gmail.com>
2023-12-02 07:59:32 -0500

    f
    f

--- roster/sign-up.php
+++ roster/sign-up.php
@@ -47,84 +47,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-<!-- The rest of the HTML and styling remains the same as the previous example -->
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css"> <!-- Include your stylesheet if needed -->
-    <style>
-       body {
-    font-family: 'Arial', sans-serif;
-    background-color: #f2f2f2;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-}
-
-.container {
-    max-width: 400px;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    text-align: center;
-}
-
-.signup-form {
-    display: flex;
-    flex-direction: column;
-}
-
-label {
-    margin-bottom: 8px;
-}
-
-input {
-    padding: 10px;
-    margin-bottom: 16px;
-}
-
-button {
-    background-color: #4287f5;
-    color: #fff;
-    padding: 12px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-}
-
-button:hover {
-    background-color: #3366cc;
-}
-
-.error-message {
-    color: #ff0000;
-    margin-bottom: 16px;
-}
-    </style>
     <title>Sign Up</title>
 </head>
 <body>
-    <h2>Sign Up</h2>
+    <div class="container">
+        <h2>Sign Up</h2>
 
-    <form method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
+        <form class="signup-form" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
 
-        <button type="submit">Sign Up</button>
-    </form>
+            <button type="submit">Sign Up</button>
+        </form>
 
-    <a href="login.php">Already have an account? Log in here</a>
+        <a href="login.php">Already have an account? Log in here</a>
+    </div>
 </body>
 </html>
